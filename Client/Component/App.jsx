39447b1782859e86c463ed2d6fe75f9669cf163c
Logan Qiu <chiu.logan@gmail.com>
2021-07-21 16:41:17 -0700

    created different folders as they are different working environment

--- Client/Component/App.jsx
+++ Client/Component/App.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-  }
+    this.state = {
 
+    };
+  }
+  
+  componentDidMount() {
+
+  }
 
   render() {
     return (
       <div> Sheri, Cameron and Ran HIA HIA HIA </div>
+      // <Overview />
+      // <Rating />
+      // <QA />
+      // <Comparison />
     )
   }
 }
