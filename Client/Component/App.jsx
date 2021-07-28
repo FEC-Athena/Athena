b70ea33297c36c1e682edd5b5e49d071234e719d
Cameron <camjuresich@gmail.com>
2021-07-27 18:54:24 -0700

    working on updated branch now

--- Client/Component/App.jsx
+++ Client/Component/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import QnA from './QnA/QnA.jsx';
-=======
 import RelatedList from './RelatedItems/RelatedList.jsx';
 import OutfitList from './RelatedItems/OutfitList.jsx';
 
->>>>>>> main
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,24 +17,13 @@ class App extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <>
-      <div> Sheri, Cameron and Ran HIA HIA HIA </div>
-      {/* // <Overview />
-      // <Rating /> */}
-      < QnA />
-
-      {/* // <Comparison /> */}
-      </>
-=======
       <div>
       {/* // <Overview />
-      // <Rating />
-      // <QA /> */}
+      // <Rating /> */}
+        <QA />
         <RelatedList />
         <OutfitList />
       </div>
->>>>>>> main
     )
   }
 }
